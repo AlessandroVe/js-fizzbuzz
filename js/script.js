c16c6b9369d1f7d2972a9182a5da6734f08a6aed
AlessandroVe <aveneziano536@gmail.com>
2021-06-01 15:22:41 +0200

    .

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 //ma per i multipli di 3 stampi “Fizz” al posto del numero e per 
 // i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli 
 //di 3 che di 5 stampi FizzBuzz.
-var value= "1";
-for(var i=1; i<100; i++){
-    value++;
-    document.getElementById("output").innerHTML= value;
+
+for(var i=1; i<=100; i++){
+
+    document.getElementById("output").innerHTML+="<br>"+ i;
+    if(i % 3 === 0 && i % 5=== 0 ){
+        document.getElementById("output").innerHTML+="<br> FizzBuzz" ;
+    }
+    else if(i % 3 === 0){
+        document.getElementById("output").innerHTML+="<br> Fizz" ;
+    }
+    else if(i % 5 === 0){
+        document.getElementById("output").innerHTML+="<br> Buzz" ;
+    }
 }
